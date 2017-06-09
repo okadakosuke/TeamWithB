058502cbe2c28bdb6667c42d94c9d9e810f22c2b
yamashita.taiki <yamashita.taiki@m.alhinc.jp>
2017-06-09 16:57:46 +0900

    taiki test

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,7 +3,13 @@ package test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+
+
 		System.out.println("test");
+		System.out.println("okadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+
+
 	}
 
 }
