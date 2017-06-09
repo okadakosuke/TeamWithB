9935acb4b3d5c23115dbec6aa92ae9fb9f9249db
okadakosuke <okada.kosuke@m.alhinc.jp>
2017-06-09 17:00:01 +0900

    okada test

--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+
+		String s = new String();
+		if(s.matches("Hello")) System.out.println(s) ;
+
 	}
 
 }
